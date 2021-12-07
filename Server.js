6a6411d5493a757f8349f02eb002175fe9c24713
Akash123Git <akashvijay769@gmail.com>
2021-12-07 14:49:35 +0530

    initail commit

---
+++ Server.js
@@ -0,0 +1,15 @@
+const http=require("http");
+const filesys=require("fs");
+
+http.createServer((req,res) => {
+res.writeHead(200,{'Content-Type':'text/html'})
+if(req.url==="/home" || req.url==="/"){
+    let page=filesys.readFileSync("static/MyPort.html");
+    res.write(page);
+}else{
+    let page=filesys.readFileSync("static/Error.html");
+    res.write(page);
+}
+
+res.end();
+}).listen(9090, () => {console.log("server has started")})
